5381bd2dc4e7079c79f28141f86200b2e81fc734
kc345ws <598043446@qq.com>
2019-10-17 19:56:08 +0800

    DB2

--- Java/DB2/src/ChcDB2/ChcGUI.java
+++ Java/DB2/src/ChcDB2/ChcGUI.java
@@ -1,7 +1,12 @@
 package ChcDB2;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileFilter;
 
 public class ChcGUI {
     private JPanel rootPanel;
@@ -18,8 +23,11 @@ public class ChcGUI {
     private JButton MoreBTN;
     private JButton CloseBTN;
     private JButton HelpBTN;
+    private JButton selectBTN;
 
     private JTable ElementTable;
+
+    public File selectedFile;
 
     public ChcGUI(){
         CenterPanel.setName("CenterPanel");
@@ -44,6 +52,46 @@ public class ChcGUI {
         MoreBTN.setEnabled(false);
         CloseBTN.setEnabled(false);
         HelpBTN.setEnabled(false);*/
+
+    }
+
+    public ChcGUI(int index){
+        CenterPanel.setName("CenterPanel");
+
+        Object[][] tableData =
+                {
+
+
+                };
+        //定义一维数据作为列标题
+        Object[] columnTitle = {"EmployeeNO" , "photo_format","photo"};
+
+        DefaultTableModel tableModel = new DefaultTableModel(tableData,columnTitle);
+        ElementTable = new JTable(tableModel);
+
+        //向中间面板添加JTable
+        CenterPanel.add(new JScrollPane(ElementTable));
+
+        /*DeleteBTN.setEnabled(false);
+        RollBackBTN.setEnabled(false);
+        FitterBTN.setEnabled(false);
+        MoreBTN.setEnabled(false);
+        CloseBTN.setEnabled(false);
+        HelpBTN.setEnabled(false);*/
+
+        selectBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jFileChooser = new JFileChooser("./");
+                FileNameExtensionFilter fileNameExtensionFilter = new FileNameExtensionFilter("图片","jpg","png","gif");
+                jFileChooser.setFileFilter(fileNameExtensionFilter);
+
+                jFileChooser.showDialog(null, "选择照片");
+                //System.out.println(showDialog);
+                selectedFile = jFileChooser.getSelectedFile();
+                System.out.println(selectedFile.getName());
+            }
+        });
 
     }
 
